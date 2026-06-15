c41735574794813352b6f5a673a154e74f013a63
erlkim <xim7.iin@gmail.com>
2026-06-15 12:56:41 +0700

    Fix build: remove invalid experimental clientPrerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -286,7 +286,7 @@ export default defineConfig({
     fallback: 'swap',
   },
   trailingSlash: 'ignore',
-  experimental: {
-    clientPrerender: 'always',
-  },
+  // experimental: {
+//   clientPrerender: 'always',
+// },
 });
